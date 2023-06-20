a5789770f88bdbdbd386119a3577b4194e2054f5
Eryk Knesz <eryk.knesz@gmail.com>
2023-06-20 23:34:59 +0200

    add Cryptographer class + docstrings for methods

--- security.py
+++ security.py
@@ -1,22 +1,43 @@
 from cryptography.fernet import Fernet
 
-try:
-    with open('key.bin', 'rb') as file_object:
-        for line in file_object:
-            key = line
-except FileNotFoundError:
-    key = Fernet.generate_key()
 
+class Cryptographer:
 
-def encrypt_psw(password):
-    cipher_suite = Fernet(key)
-    ciphered_psw = cipher_suite.encrypt(password.encode())
-    return ciphered_psw
+    def __init__(self):
+        self.key = self._get_key()
 
+    def _get_key(self):
+        """
+        Get a key used by the Cryptography library for en- and decryption.
 
-def decrypt_psw(password):
-    cipher_suite = Fernet(key)
-    ciphered_psw = password.encode()
-    unciphered_psw = cipher_suite.decrypt(ciphered_psw)
-    return unciphered_psw
+        :return str: key
+        """
+        try:
+            with open('key.bin', 'rb') as file_object:
+                for line in file_object:
+                    key = line
+        except FileNotFoundError:
+            key = Fernet.generate_key()
+        return key
+
+    def encrypt_psw(self, password):
+        """
+        Encrypt a password.
+
+        :param str password: password to encrypt
+        :return str: encrypted password
+        """
+        cipher_suite = Fernet(self.key)
+        return cipher_suite.encrypt(password.encode())
+
+    def decrypt_psw(self, password):
+        """
+        Decrypt a password.
+
+        :param str password: password to decrypt
+        :return str: decrypted password
+        """
+        cipher_suite = Fernet(self.key)
+        ciphered_psw = password.encode()
+        return cipher_suite.decrypt(ciphered_psw)
 
