f617a17c853ccc033e018f22b1c277ab8e022ac2
Eryk Knesz <eryk.knesz@gmail.com>
2023-07-26 22:56:27 +0200

    add password generation feature

--- gui.py
+++ gui.py
@@ -50,7 +50,7 @@ class GUI(tkinter.Tk):
                                                    width=14,
                                                    background=BTN_INFO_BG,
                                                    activebackground=BTN_INFO_BG_ACTIVE,
-                                                   command="generate_password")
+                                                   command=self.generate_password)
         self.generate_pass_button.grid(row=3, column=2)
         self.add_button = tkinter.Button(text="Add",
                                          width=21,
@@ -202,3 +202,20 @@ class GUI(tkinter.Tk):
         commands.UpdatePasswordCommand().execute(data)
         self.table.item(selected_item,
                         values=(obj_id,) + tuple(data.values()))
+
+    def generate_password(self):
+        letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                   'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+                   'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+        numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+        password_letters = [choice(letters) for _ in range(randint(8, 10))]
+        password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+        password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+        password_list = password_letters + password_symbols + password_numbers
+        shuffle(password_list)
+
+        password = "".join(password_list)
+        self.password_entry.insert(0, password)
